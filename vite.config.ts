98e872949b0e63718d98d7accf66620e6225cf39
Ayush mahapatra <ayushmahapatra98@gmail.com>
2026-05-27 18:24:58 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,3 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  vite: {
-    ssr: {
-      noExternal: true,
-    },
-  },
-});
+export default defineConfig({});
