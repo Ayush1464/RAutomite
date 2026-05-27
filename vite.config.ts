2f405fbcc512be6d80f60e2b16c7f2e75ce160f2
Ayush mahapatra <ayushmahapatra98@gmail.com>
2026-05-27 18:07:23 +0530

    Fix SSR deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,16 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig();
+export default defineConfig(
+    {
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart(),
+    
+  ],
+}
+);
